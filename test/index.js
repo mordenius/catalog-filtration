5a668e6be322ce9c2919cbb1122d73e3edf5d33d
Mordenius <mordenius@gmail.com>
2017-03-29 17:55:57 +0300

    test(test) simple test with db file

--- test/index.js
+++ test/index.js
@@ -1,1 +1,33 @@
-import CatalogFiltration from '~/';
+import Catalog from '~/';
+import Goods from './out_goods.json';
+
+let options = {
+	goods: Goods,
+	filterFields: ["VID_TOVARA_UA", "CVET_UA", "BRAND", "PRICE_ACTION", "SEASON_UA", "STYLE_UA", "PODKL_UA", 
+			"VERX_UA", "PODOSHVA_UA", "TECHNOLOGY", "GENDER_UA", "ACCESSORY", "UNIQUE", "UNIQUE_SEASON"],
+	priceField: 'PRICE_ACTION',
+	presetsRules: {
+		'female': {"GENDER_UA": ["Жіноче"], "ACCESSORY": ["N"]},
+		'male': {"GENDER_UA": ["Чоловіче"], "ACCESSORY": ["N"]} 
+	}
+}
+
+let catalog = new Catalog(options);
+
+console.time('FILTER_INIT');
+
+let selected = {
+	BRAND: ['Attipas', 'Braska'],
+	SEASON_UA: ['Осінь-Зима 2016', 'Осінь-Зима 2015'],
+	TODAY: []
+}
+// let selected = 'female';
+catalog.filter(selected)
+	.then(getStores)
+	.then(() => console.timeEnd('FILTER_INIT'))
+
+
+function getStores(stores){
+	// console.log(stores.availableFilters);
+	// console.log(stores.filterMap.TECHNOLOGY);
+}
