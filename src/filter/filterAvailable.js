b0c962e96fc668f8d86d8661e34028dc2d3ceefe
mordenius <mordenius@gmail.com>
2017-07-08 02:07:34 +0300

    fix(listing/filterAvailable) when preset active fix(listing/filterAvailable) handling for category in the selectedFilter refactor(listing/filterAvailable) prettier

--- src/filter/filterAvailable.js
+++ src/filter/filterAvailable.js
@@ -2,58 +2,59 @@ import _ from "lodash";
 import FilterListCollector from "~/listing/filterListCollector";
 
 class FilterAvailable {
-  constructor(options) {
-    this.stores = options.stores;
-    this.goods = options.goods;
-    this.filterListCollector = new FilterListCollector(options);
+	constructor(options) {
+		this.stores = options.stores;
+		this.goods = options.goods;
+		this.filterListCollector = new FilterListCollector(options);
 
-    this.subscribe();
-  }
+		this.subscribe();
+	}
 
-  subscribe() {
-    this.stores.productList.subscribe(() => {
-      this.available();
-    });
-  }
+	subscribe() {
+		this.stores.productList.subscribe(() => {
+			this.available();
+		});
+	}
 
-  available() {
-    console.time("AVAILABLE");
+	available() {
+		console.time("AVAILABLE");
 
-    let preset = this.stores.catalog.getStore.preset;
-    let condition =
-      0 < Object.keys(this.stores.selectedFilters.getStore).length;
-    let available = condition
-      ? this.availableCommon()
-      : this.availablePreset(preset);
+		let preset = this.stores.catalog.getStore.preset;
+		let condition =
+			0 < Object.keys(this.stores.selectedFilters.getStore).length;
+		let available = condition
+			? this.availableCommon()
+			: this.availablePreset(preset);
 
-    this.stores.availableFilters.set(available);
+		this.stores.availableFilters.set(available);
 
-    console.timeEnd("AVAILABLE");
-  }
+		console.timeEnd("AVAILABLE");
+	}
 
-  availablePreset(preset) {
-    return this.stores.filterMap.getStore.presets[preset].available;
-  }
+	availablePreset(preset) {
+		return _.clone(this.stores.filterMap.getStore.presets[preset].available);
+	}
 
-  availableCommon() {
-    const available = {};
+	availableCommon() {
+		const preset = this.stores.catalog.getStore.preset;
+		const available = (preset) ? this.availablePreset(preset) : {};
 
-    const map = this.stores.filterMap.getStore.map;
-    const selectedFilters = this.stores.selectedFilters.getStore;
+		const map = this.stores.filterMap.getStore.map;
+		const selectedFilters = this.stores.selectedFilters.getStore;
 
-    _.map(selectedFilters, (values, category) => {
-      _.map(values, value => {
-        if (typeof map[category][value] === "object") {
-          _.map(map[category][value].available, (items, key) => {
-            if (false === _.has(available, key)) available[key] = [];
-            available[key] = _.union(available[key], items);
-          });
-        }
-      });
-    });
+		_.map(selectedFilters, (values, category) => {
+			_.map(values, value => {
+				if (typeof map[category][value] === "object") {
+					_.map(map[category][value].available, (items, key) => {
+						if (false === _.has(available, key)) available[key] = items;
+						else available[key] = _.intersection(available[key], items);
+					});
+				}
+			});
+		});
 
-    return available;
-  }
+		return available;
+	}
 }
 
 export default FilterAvailable;
