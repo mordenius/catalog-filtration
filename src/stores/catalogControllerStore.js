32192d37dc95f1b337f1dacb971fadd4e8e3c649
mordenius <mordenius@gmail.com>
2017-07-13 02:44:55 +0300

    dev(stores) update redux-store-controller

--- src/stores/catalogControllerStore.js
+++ src/stores/catalogControllerStore.js
@@ -1,25 +1,27 @@
 import { StoreClass } from "redux-store-controller";
 
 class CatalogControllerStore extends StoreClass {
-  constructor() {
-    super({ preset: "all" });
-  }
+	constructor() {
+		super({
+			initState: { preset: "all" }
+		});
+	}
 
-  update(state, action) {
-    switch (action.type) {
-      case "PRESET":
-        state.preset = action.preset;
-        break;
-      default:
-        return state;
-    }
+	update(state, action) {
+		switch (action.type) {
+			case "PRESET":
+				state.preset = action.preset;
+				break;
+			default:
+				return state;
+		}
 
-    return state;
-  }
+		return state;
+	}
 
-  preset(preset) {
-    this._store.dispatch({ type: "PRESET", preset: preset });
-  }
+	preset(preset) {
+		this._store.dispatch({ type: "PRESET", preset: preset });
+	}
 }
 
 export default CatalogControllerStore;
--- src/stores/filterMapStore.js
+++ src/stores/filterMapStore.js
@@ -1,32 +1,34 @@
 import { StoreClass } from "redux-store-controller";
 
 class FilterMapStore extends StoreClass {
-  constructor() {
-    super({ map: null, presets: null });
-  }
+	constructor() {
+		super({
+			initState: { map: null, presets: null }
+		});
+	}
 
-  update(state, action) {
-    switch (action.type) {
-      case "MAP":
-        state.map = action.map;
-        break;
-      case "PRESETS":
-        state.presets = action.presets;
-        break;
-      default:
-        return state;
-    }
+	update(state, action) {
+		switch (action.type) {
+			case "MAP":
+				state.map = action.map;
+				break;
+			case "PRESETS":
+				state.presets = action.presets;
+				break;
+			default:
+				return state;
+		}
 
-    return state;
-  }
+		return state;
+	}
 
-  setFilterMap(map) {
-    this._store.dispatch({ type: "MAP", map: map });
-  }
+	setFilterMap(map) {
+		this._store.dispatch({ type: "MAP", map: map });
+	}
 
-  setFilterPresets(presets) {
-    this._store.dispatch({ type: "PRESETS", presets: presets });
-  }
+	setFilterPresets(presets) {
+		this._store.dispatch({ type: "PRESETS", presets: presets });
+	}
 }
 
 export default FilterMapStore;
